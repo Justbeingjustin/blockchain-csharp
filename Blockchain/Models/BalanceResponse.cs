6fccc5d2f3b8a2cf351e932415bf78ddccf2a317
Justin Gerber <Justbeingjustin@aol.com>
2019-06-03 20:53:02 -0700

    initial project push

---
+++ Blockchain/Models/BalanceResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Blockchain.Models
+{
+    public class BalanceResponse
+    {
+        [JsonIgnore]
+        public string Address { get; set; }
+
+        [JsonProperty("final_balance")]
+        public long FinalBalance { get; set; }
+
+        [JsonProperty("n_tx")]
+        public long NumberOfTransactions { get; set; }
+
+        [JsonProperty("total_received")]
+        public long TotalReceived { get; set; }
+    }
+}
---
+++ Blockchain/Services/WalletRepository.cs
@@ -0,0 +1,36 @@
+﻿using Blockchain.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blockchain.Services
+{
+    public class WalletRepository
+    {
+        private readonly string _apiKey;
+        private readonly string _address;
+        private string baseUrl = @"https://blockchain.info/balance?active=";
+
+        public WalletRepository(string apiKey, string address)
+        {
+            _apiKey = apiKey;
+            _address = address;
+        }
+
+        public BalanceResponse GetBalance()
+        {
+            var client = new RestClient(baseUrl + _address + "&password=" + _apiKey);
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            var balanceResponse = JsonConvert.DeserializeObject<Dictionary<string, BalanceResponse>>(response.Content).First();
+            return new BalanceResponse()
+            {
+                FinalBalance = balanceResponse.Value.FinalBalance,
+                NumberOfTransactions = balanceResponse.Value.NumberOfTransactions,
+                TotalReceived = balanceResponse.Value.TotalReceived,
+                Address = balanceResponse.Key
+            };
+        }
+    }
+}
